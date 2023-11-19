ac2a819f3ab5d9f6d328c34384230f4045c731cd
Jitendra (Jitu) Kumar <kumarj@ornl.gov>
2023-11-19 18:30:00 -0500

    script to analyze ndre and shap at two drought deciduous sites.

---
+++ src/era5/drought_deciduous_phenology.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import numpy as np
+
+# Read the list of deciduous sites and their NDRE time series
+dsites_data = pd.read_csv("/data1/jbk/projects/climate/tropics/marcos_longo/drought_deciduous_sites_MarcosLongo.ndre", delimiter=" ")
+
+# extract NDRE timesereis for BCI - 25 x 6 = 150 values
+bci_ndre = dsites_data[dsites_data.code == "BCI"].loc[:, dsites_data.columns.str.contains('n20')]
+
+# create list of dates
+dates = pd.date_range('2017-01-01', periods=25, freq='15D')
+dates = dates.union(pd.date_range('2018-01-01', periods=25, freq='15D'))
+dates = dates.union(pd.date_range('2019-01-01', periods=25, freq='15D'))
+dates = dates.union(pd.date_range('2020-01-01', periods=25, freq='15D'))
+dates = dates.union(pd.date_range('2021-01-01', periods=25, freq='15D'))
+dates = dates.union(pd.date_range('2022-01-01', periods=25, freq='15D'))
+
+plt.plot(dates, bci_ndre.to_numpy()[0])
