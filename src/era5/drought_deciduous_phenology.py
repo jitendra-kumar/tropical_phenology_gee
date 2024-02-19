b5c880c7b552918280241b4800a4820f5f5a0f97
Jitendra Kumar <jkumar@climatemodeling.org>
2024-02-19 13:26:11 -0500

    updates to phenometrics calculation notebook.

--- src/era5/drought_deciduous_phenology.py
+++ src/era5/drought_deciduous_phenology.py
@@ -1,11 +1,16 @@
 import pandas as pd 
 import numpy as np
+import matplotlib.pyplot as plt 
 
 # Read the list of deciduous sites and their NDRE time series
-dsites_data = pd.read_csv("/data1/jbk/projects/climate/tropics/marcos_longo/drought_deciduous_sites_MarcosLongo.ndre", delimiter=" ")
+#landscape
+#dsites_data = pd.read_csv("/data1/jbk/projects/climate/tropics/marcos_longo/drought_deciduous_sites_MarcosLongo.ndre", delimiter=" ")
+#asha
+dsites_data = pd.read_csv("/home/jbk/projects/climate/tropics/marcos_longo/drought_deciduous_sites_MarcosLongo.ndre", delimiter="|")
 
 # extract NDRE timesereis for BCI - 25 x 6 = 150 values
 bci_ndre = dsites_data[dsites_data.code == "BCI"].loc[:, dsites_data.columns.str.contains('n20')]
+pnm_ndre = dsites_data[dsites_data.code == "PNM"].loc[:, dsites_data.columns.str.contains('n20')]
 
 # create list of dates
 dates = pd.date_range('2017-01-01', periods=25, freq='15D')
@@ -16,3 +21,5 @@ dates = dates.union(pd.date_range('2021-01-01', periods=25, freq='15D'))
 dates = dates.union(pd.date_range('2022-01-01', periods=25, freq='15D'))
 
 plt.plot(dates, bci_ndre.to_numpy()[0])
+plt.ylim([0,1])
+plt.show()
